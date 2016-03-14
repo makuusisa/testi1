db8947175d1fe65cd34fedbe839e7725dfe5d870
Marko Kuusisalmi <Marko Kuusisalmi@130.230.155.187>
2016-03-14 10:37:08 +0200

    Testaus_2

--- GitHub_testaus/src/Github_testi_1.java
+++ GitHub_testaus/src/Github_testi_1.java
@@ -4,6 +4,7 @@ public class Github_testi_1 {
 	public static void main(String[] args) {
 		System.out.println("maanantai 14.3.2016");
 		System.out.println("Toimiiko?");
+		System.out.println("toinen Commit");
 	}
 
 }
